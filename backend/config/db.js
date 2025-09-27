8778fa8790ecf67f10fecae75344f6af57dc9df0
Gouse-ux <234g1a3339@srit.ac.in>
2025-09-27 22:38:56 +0530

    updated file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		console.log("Mongodb is connected successfully 👌")
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 		process.exit(1); // process code 1 code means exit with failure, 0 means success
